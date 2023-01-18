d0cb712a15b07e490e257948ad7ce3be18bc8156
vini <vinicius.medeiros@dataside.com.br>
2023-01-18 15:36:05 -0300

    feat: create project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,51 @@
-import { Button, Flex, Heading, Stack } from '@chakra-ui/react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
+import { Button, Flex, Image } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction, useRef } from 'react';
+import { AvatarGrid } from '../components/AvatarGrid';
+import { Dataholic } from '../components/Dataholic';
+import { useReactToPrint } from 'react-to-print';
+import React from 'react';
+import { ContentToPrint } from '../components/ContentToPrint';
 
-export default function Home() {
-	const axiosInstance = axios.create({
-		baseURL: 'http://localhost:3333',
+const images = [
+	{
+		id: '1',
+		src: '/vinidev.png',
+		name: 'Vinicius Medeiros',
+		department: 'development',
+	},
+	{
+		id: '2',
+		src: '/vinidev.png',
+		name: 'Vinicius Medeiros',
+		department: 'development',
+	},
+	{
+		id: '3',
+		src: '/vinidev.png',
+		name: 'Vinicius Medeiros',
+		department: 'development',
+	},
+	{
+		id: '4',
+		src: '/vinidev.png',
+		name: 'Vinicius Medeiros',
+		department: 'development',
+	},
+];
+
+export default function Page() {
+	const componentRef = useRef(null);
+
+	const handlePrint = useReactToPrint({
+		content: () => componentRef.current,
 	});
 
-	const [count, setCount] = useState(0);
-
-	useEffect(() => {
-		(async () => {
-			const { data } = await axiosInstance.get('/counters');
-
-			setCount(data);
-		})();
-	}, []);
-
-	const handleCounter = async () => {
-		const { data } = await axiosInstance.put('/counters');
-		setCount(data);
-	};
-
 	return (
-		<Flex direction="column" align="center" justify="center">
-			<title>Tue</title>
-			<video
-				src="/tue.mp4"
-				autoPlay
-				muted
-				style={{
-					width: '100%',
-					height: '100vh',
-					objectFit: 'cover',
-				}}
-			/>
-			<Stack spacing="6" position="absolute">
-				<Heading p="2" bg="blackAlpha.500">
-					{count} pedidos
-				</Heading>
-				<Button onClick={handleCounter} colorScheme="whatsapp">
-					Pedir
-				</Button>
-			</Stack>
+		<Flex h="100vh" align="center" justify="center" direction="column">
+			<ContentToPrint images={images} ref={componentRef} />
+			<Button colorScheme="blue" onClick={handlePrint} mt="2rem">
+				Printar
+			</Button>
 		</Flex>
 	);
 }
